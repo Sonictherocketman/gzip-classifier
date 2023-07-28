56980ff52a7a32f3ecfb444ddbbe9e7e77e2be25
Brian Schrader <brian@brianschrader.com>
2023-07-28 14:01:21 -0700

    Adds new Quick Parallel Classifier

--- gzip_classifier/__init__.py
+++ gzip_classifier/__init__.py
@@ -1,2 +1,2 @@
 from .classifier import Classifier, ParallelClassifier  # noqa
-from .quick import QuickClassifier  # noqa
+from .quick import QuickClassifier, QuickParallelClassifier  # noqa
--- gzip_classifier/quick.py
+++ gzip_classifier/quick.py
@@ -1,9 +1,10 @@
 from gzip import compress
 
-from .classifier import Classifier
+from .classifier import Classifier, ParallelClassifier
 from .utils import (
     prepare_input,
     calc_distance,
+    calc_distance_w_args,
     generate_index,
     get_likely_bin,
     add_overscan,
@@ -19,6 +20,13 @@ class QuickClassifier(Classifier):
 
     # TODO: Add import/export or recalc for importing models.
 
+    def get_indicies(self, Cx1):
+        return add_overscan(
+            *get_likely_bin(self._index, Cx1),
+            bound=len(self._model),
+            overscan=self.overscan,
+        )
+
     def train(self, training_data, labels):
         super().train(training_data, labels)
         self._index = generate_index(self._model)
@@ -29,14 +37,29 @@ class QuickClassifier(Classifier):
         x1 = prepare_input(sample)
         Cx1 = len(compress(x1))
 
-        start, stop = add_overscan(
-            *get_likely_bin(self._index, Cx1),
-            bound=len(self._model),
-            overscan=self.overscan,
-        )
+        start, stop = self.get_indicies(Cx1)
         candidates = sorted((
             (calc_distance(x1, Cx1, x2, Cx2), label)
             for x2, _, Cx2, label in self._model[start:stop]
         ), key=lambda x: x[0])
+
+        return self._tabluate(candidates, k, include_all=include_all)
+
+
+class QuickParallelClassifier(QuickClassifier, ParallelClassifier):
+
+    def classify(self, sample, k=None, include_all=True, overscan=None):
+        k = k if k else self.k
+
+        x1 = prepare_input(sample)
+        Cx1 = len(compress(x1))
+
+        start, stop = self.get_indicies(Cx1)
+        values = (
+            (x1, Cx1, x2, Cx2, label)
+            for x2, _, Cx2, label in self._model[start:stop]
+        )
+        results = self.pool.imap(calc_distance_w_args, values, self.chunksize)
+        candidates = sorted(results, key=lambda x: x[0])
 
         return self._tabluate(candidates, k, include_all=include_all)
